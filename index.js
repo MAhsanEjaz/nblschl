907e3e1eca117a19b90860054ba7f7f594a26118
ahsan <ahsanejaz99@gmail.com>
2022-12-07 15:57:16 +0500

    db

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose')
-const Router = require('./routes/routes')
+const Router = require('./routes/routes');
+const e = require('express');
 const app = express();
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+
 
 app.get('/tahir',(req,res)=>{
     MongoClient.connect('mongodb+srv://tahir:tahir12345@cluster0.vdllyev.mongodb.net/?retryWrites=true&w=majority', function(err, db) {
         if (err) throw err;
-        var dbo = db.db("sample_training");
+        var dbo = db.db("tahir");
         //Find all documents in the customers collection:
-        dbo.collection("routes").find({}).toArray(function(err, result) {
+        dbo.collection("tt").find({}).toArray(function(err, result) {
           if (err) throw err;
           console.log(result);
           db.close();
@@ -32,11 +33,12 @@ app.get('/tahir',(req,res)=>{
 //     })
 // })
 
-mongoose.connect('mongodb://localhost:27017',{useNewUrlParser: true},(err, result)=>{
-
-    if(err) throw console.error();
-    // database = db.db("tahir");
-    console.log("Connected")
+mongoose.connect(process.env.DATABASE|| 'mongodb+srv://tahir:tahir12345@cluster0.vdllyev.mongodb.net/?retryWrites=true&w=majority',(err)=>{
+    if(!err){
+        console.log('Connected')
+    }else{
+        console.log('Not Connected')
+    }
 })
 
 
