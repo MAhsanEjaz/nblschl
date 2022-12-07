2a317ebd00c8cce8520fc3ce441ec96e82841ef0
ahsan <ahsanejaz99@gmail.com>
2022-12-07 15:06:56 +0500

    sds

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const app = express();
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
-
-
 app.get('/tahir',(req,res)=>{
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
